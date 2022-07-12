42b6eb691f2be052f6b894102f286f4bc55ff900
Ian <iangarciag@fb.com>
2022-07-12 15:49:37 -0700

    [API] Implemented Bid table schema.

---
+++ api/database/migrations/2022_07_11_225840_create_bids_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bids', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('post_id')->references('id')->on('posts')->cascadeOnDelete();
+            $table->foreignId('offerer_id')->references('id')->on('offerers')->cascadeOnDelete();
+            $table->bigInteger('amount');
+            $table->string('currency');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bids');
+    }
+};
