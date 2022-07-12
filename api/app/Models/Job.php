31bfd545c88b8a53f15d79e32452d8a1ddfd9234
Ian <iangarciag@fb.com>
2022-07-12 13:09:13 -0700

    [API] Added Job model relations.
    Job model.

---
+++ api/app/Models/Job.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
+    use HasFactory;
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function offerer(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Offerer::class, 'offerer_id');
+    }
+
+    public function address(): \Illuminate\Database\Eloquent\Relations\HasOne
+    {
+        return $this->hasOne(Address::class);
+    }
+
+    public function rate(): \Illuminate\Database\Eloquent\Relations\HasOne
+    {
+        return $this->hasOne(Rate::class, 'rate_id');
+    }
+
+    public function logs(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(Logs::class);
+    }
+}
