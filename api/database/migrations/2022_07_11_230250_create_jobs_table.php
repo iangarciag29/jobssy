70b326e95a681b5247dfd2b47ff19c2bfc1df66e
Ian <iangarciag@fb.com>
2022-07-12 15:50:17 -0700

    [API] Implemented Job table schema.

---
+++ api/database/migrations/2022_07_11_230250_create_jobs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('rate_id')->references('id')->on('rates')->cascadeOnDelete();
+            $table->foreignId('user_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->foreignId('offerer_id')->references('id')->on('offerers')->cascadeOnDelete();
+            $table->text('description');
+            $table->bigInteger('price');
+            $table->string('currency');
+            $table->integer('state');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
