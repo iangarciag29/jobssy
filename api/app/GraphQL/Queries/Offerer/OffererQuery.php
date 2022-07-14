0ee04a72795402a888ec5dd410f0dd8354c8d7e8
Ian <iangarciag@fb.com>
2022-07-14 14:27:20 -0700

    [API] [GRAPHQL] Implemented single offerer model query.

---
+++ api/app/GraphQL/Queries/Offerer/OffererQuery.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\GraphQL\Queries\Offerer;
+
+use App\Models\Offerer;
+use GraphQL\Type\Definition\Type;
+use GraphQL\Type\Definition\Type as GraphQLType;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Rebing\GraphQL\Support\Query;
+
+class OffererQuery extends Query
+{
+
+    protected $attributes = [
+        'name' => 'offerer'
+    ];
+
+    public function type(): GraphQLType
+    {
+        return GraphQL::type('Offerer');
+    }
+
+    public function args(): array
+    {
+        return [
+            'id' => [
+                'name' => 'id',
+                'type' => Type::nonNull(Type::int()),
+                'rules' => ['required']
+            ]
+        ];
+    }
+
+    public function resolve($root, $args)
+    {
+        return Offerer::findOrFail($args['id']);
+    }
+}
