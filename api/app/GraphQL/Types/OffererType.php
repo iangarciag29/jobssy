c0cd9fd0485973ef2dd393ad7c058495a5b0b108
Ian <iangarciag@fb.com>
2022-07-14 14:28:00 -0700

    [API] [GRAPHQL] Implemented Offerer model type.

---
+++ api/app/GraphQL/Types/OffererType.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\GraphQL\Types;
+
+
+use App\Models\Offerer;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Rebing\GraphQL\Support\Type as GraphQLType;
+
+class OffererType extends GraphQLType
+{
+
+    protected $attributes = [
+        'name' => 'Offerer',
+        'description' => 'User\'s offerer profile.',
+        'model' => Offerer::class
+    ];
+
+    public function fields(): array
+    {
+        return [
+            'id' => [
+                'type' => Type::nonNull(Type::int()),
+                'description' => 'ID of the offerer profile.'
+            ],
+            'rating' => [
+                'type' => Type::nonNull(Type::int()),
+                'description' => 'Offerer\'s rating.'
+            ],
+            'user' => [
+                'type' => GraphQL::type('User'),
+                'description' => 'Offerer\'s user details.'
+            ],
+            'bids' => [
+                'type' => Type::listOf(GraphQL::type('Bid')),
+                'description' => 'Offerer\'s placed bids.'
+            ],
+            'rate' => [
+                'type' => GraphQL::type('Rate'),
+                'description' => 'Offerer\'s rates.'
+            ],
+            'start_time' => [
+                'type' => Type::nonNull(Type::string()),
+                'description' => 'Profile creation date.'
+            ],
+            'jobs_completed' => [
+                'type' => Type::nonNull(Type::int()),
+                'description' => 'Amount of jobs completed.'
+            ],
+            'created_at' => [
+                'type' => Type::nonNull(Type::string()),
+                'description' => 'Created at date.'
+            ],
+            'updated_at' => [
+                'type' => Type::nonNull(Type::string()),
+                'description' => 'Updated at date.'
+            ],
+        ];
+    }
+
+}
