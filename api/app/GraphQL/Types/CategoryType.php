25c53e073460f9b628fef1d781abf335a3c10d04
Ian <iangarciag@fb.com>
2022-07-14 13:10:36 -0700

    [API] [GRAPHQL] Designed Category model type.

---
+++ api/app/GraphQL/Types/CategoryType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\GraphQL\Types;
+
+use App\Models\Category;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Rebing\GraphQL\Support\Type as GraphQLType;
+
+class CategoryType extends GraphQLType
+{
+
+    protected $attributes = [
+        'name' => 'Category',
+        'description' => 'Offerer service category.',
+        'model' => Category::class
+    ];
+
+
+    public function fields(): array
+    {
+        return [
+            'id' => [
+                'type' => Type::nonNull(Type::int()),
+                'description' => 'ID of the category.'
+            ],
+            'name' => [
+                'type' => Type::nonNull(Type::string()),
+                'description' => 'Name of the category.'
+            ],
+            'services' => [
+                'type' => Type::listOf(GraphQL::type('Service')),
+                'description' => 'List of all category services.'
+            ],
+            'created_at' => [
+                'type' => Type::nonNull(Type::string()),
+                'description' => 'Bid creation date.'
+            ],
+            'updated_at' => [
+                'type' => Type::nonNull(Type::string()),
+                'description' => 'Last time bid was updated.'
+            ]
+        ];
+    }
+}
