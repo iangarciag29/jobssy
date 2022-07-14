ad32a1c7ebc409002d8d09d53da7c982724c8f9c
Ian <iangarciag@fb.com>
2022-07-14 13:04:50 -0700

    [API] [GRAPHQL] Added single bid query.

---
+++ api/app/GraphQL/Queries/Bid/BidQuery.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\GraphQL\Queries\Bid;
+
+use App\Models\User;
+use GraphQL\Type\Definition\Type;
+use GraphQL\Type\Definition\Type as GraphQLType;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Rebing\GraphQL\Support\Query;
+
+class BidQuery extends Query
+{
+
+    protected $attributes = [
+        'name' => 'bid'
+    ];
+
+    public function type(): GraphQLType
+    {
+        return GraphQL::type('Bid');
+    }
+
+    public function args(): array
+    {
+        return [
+            'id' => [
+                'name' => 'id',
+                'type' => Type::nonNull(Type::int()),
+                'rules' => ['required']
+            ]
+        ];
+    }
+
+    public function resolve($root, $args)
+    {
+        return User::findOrFail($args['id']);
+    }
+}
