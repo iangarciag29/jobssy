a4e1b678fcc2d19ed6669744153066f21c82a9f6
Ian <iangarciag@fb.com>
2022-07-14 13:06:05 -0700

    [API] [GRAPHQL] Implemented Bid type.

---
+++ api/app/GraphQL/Types/BidType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\GraphQL\Types;
+
+use App\Models\Bid;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Rebing\GraphQL\Support\Type as GraphQLType;
+
+class BidType extends GraphQLType
+{
+
+    protected $attributes = [
+        'name' => 'Bid',
+        'description' => 'Bid object used for posts.',
+        'model' => Bid::class
+    ];
+
+    public function fields(): array
+    {
+        return [
+            'id' => [
+                'type' => Type::nonNull(Type::int()),
+                'description' => 'ID of the bid.'
+            ],
+            'offerer' => [
+                'type' => GraphQL::type('Offerer'),
+                'description' => 'Bid offerer.'
+            ],
+            'amount' => [
+                'type' => Type::nonNull(Type::float()),
+                'description' => 'Bid amount.'
+            ],
+            'currency' => [
+                'type' => Type::nonNull(Type::string()),
+                'description' => 'Currency.'
+            ],
+            'created_at' => [
+                'type' => Type::nonNull(Type::string()),
+                'description' => 'Bid creation date.'
+            ],
+            'updated_at' => [
+                'type' => Type::nonNull(Type::string()),
+                'description' => 'Last time bid was updated.'
+            ]
+        ];
+    }
+}
