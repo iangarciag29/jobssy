4f594e4f9e5712ad88422c99583efb5aa1f527de
Ian <iangarciag@fb.com>
2022-07-18 18:30:35 -0700

    [CLIENT] Implemented accessibility feature.
    Announces new page on route change.

---
+++ client/src/components/Accessibility/PageSwitchAccessibility.tsx
@@ -0,0 +1,22 @@
+import {useEffect, useState} from "react";
+import {useLocation} from "react-router-dom";
+
+const PageSwitchAccessibility = (): JSX.Element => {
+
+    const [announcement, setAnnouncement] = useState<string>("");
+    const location = useLocation();
+
+    useEffect(() => {
+        if (location.pathname.slice(1)) {
+            setTimeout(() => setAnnouncement(`Navigated to ${location.pathname.slice(1)} page.`), 500);
+        } else {
+            setAnnouncement("");
+        }
+    }, [location]);
+
+    return <span className="sr-only" role="status" aria-live="polite" aria-atomic={true}>
+        {announcement}
+    </span>
+}
+
+export default PageSwitchAccessibility;
