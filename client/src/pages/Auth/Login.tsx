c9c026e4f5d61e9ce8eaecdde195b7bc717ee9d4
Ian <iangarciag@fb.com>
2022-07-18 18:35:45 -0700

    [CLIENT] Initial auth pages.

---
+++ client/src/pages/Auth/Login.tsx
@@ -0,0 +1,7 @@
+const Login = (): JSX.Element => {
+    return <div>
+        <h1>Login PAGE.</h1>
+    </div>
+}
+
+export default Login;
---
+++ client/src/pages/Auth/SignUp.tsx
@@ -0,0 +1,7 @@
+const SignUp = (): JSX.Element => {
+    return <div>
+        <h1>SignUp PAGE</h1>
+    </div>
+}
+
+export default SignUp;
