4f0da191ec66080db928017de507acde61c9ed85
Ian <iangarciag@fb.com>
2022-07-18 18:35:34 -0700

    [CLIENT] Initial redux store.

---
+++ client/src/store/index.ts
@@ -0,0 +1,15 @@
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
+import thunk from "redux-thunk";
+
+// @ts-ignore
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({});
+
+// TODO: createStore is deprecated, the new version will be implemented in the future.
+const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk))
+);
+
+export default store;
