646bed83a0446378269c18347b91f1069177d2e1
Ian <iangarciag@fb.com>
2022-07-18 18:30:04 -0700

    [CLIENT] Added initial version of pages.

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import {Spinner} from "flowbite-react";
+import React, {useState} from "react";
+
+const Home = (): JSX.Element => {
+
+    const [open, setOpen] = useState(false);
+
+    return <div>
+        <h1>HOME</h1>
+    </div>
+}
+
+export default Home;
