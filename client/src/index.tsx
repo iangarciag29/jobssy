16b7ac6dd9d39cbee88ebc4966e8708cb66e43ca
Ian <iangarciag@fb.com>
2022-07-18 18:35:06 -0700

    [CLIENT] Added redux provider.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,19 +1,26 @@
-import React, {Suspense} from 'react';
+import React, {StrictMode, Suspense} from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/css/index.css';
 import App from './App';
 import {RelayEnvironmentProvider} from "react-relay";
 import relayEnvironment from './relay/RelayEnvironment';
+import {SidebarProvider} from "./context/SidebarContext";
+import {Provider} from "react-redux";
+import store from "./store";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <RelayEnvironmentProvider environment={relayEnvironment}>
-        <React.StrictMode>
-            <Suspense fallback={<h1>LOADING...</h1>}>
-                <App/>
-            </Suspense>
-        </React.StrictMode>
-    </RelayEnvironmentProvider>
+    <Provider store={store}>
+        <RelayEnvironmentProvider environment={relayEnvironment}>
+            <StrictMode>
+                <SidebarProvider>
+                    <Suspense fallback={<h1>LOADING...</h1>}>
+                        <App/>
+                    </Suspense>
+                </SidebarProvider>
+            </StrictMode>
+        </RelayEnvironmentProvider>
+    </Provider>
 );
