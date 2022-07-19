ddd451d90478f21e62a85f6be27d260ab7a47b41
Ian <iangarciag@fb.com>
2022-07-18 18:29:07 -0700

    [CLIENT] Implemented route structure.
    Enables dynamic routing on the layout component.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {lazy} from 'react';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import PageSwitchAccessibility from "./components/Accessibility/PageSwitchAccessibility";
+
+const Landing = lazy(() => import('./pages/Landing'));
+const Login = lazy(() => import('./pages/Auth/Login'));
+const SignUp = lazy(() => import('./pages/Auth/SignUp'));
+const Layout = lazy(() => import('./containers/Layout'));
 
 function App(): JSX.Element {
     return (
-        <div className="text-center mt-20 flex flex-col space-y-10">
-            <h1 className="uppercase text-5xl font-black">Jobssy</h1>
-            <p>We are under construction. Please come back soon</p>
-        </div>
+        <>
+            <Router>
+                <PageSwitchAccessibility/>
+                <Routes>
+                    <Route path="/" element={<Landing/>}/>
+                    <Route path="/login" element={<Login/>}/>
+                    <Route path="/signup" element={<SignUp/>}/>
+                    <Route path="/app/*" element={<Layout/>}/>
+                </Routes>
+            </Router>
+        </>
     );
 }
 
