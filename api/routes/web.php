b3568f165b6623e5e0808516ee37f2a81cd3a56e
Ian <iangarciag@fb.com>
2022-07-19 18:36:05 -0700

    [API] [AUTH] Renamed application.

--- api/routes/web.php
+++ api/routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return ['Jobssy API' => app()->version()];
 });
 
 require __DIR__.'/auth.php';
