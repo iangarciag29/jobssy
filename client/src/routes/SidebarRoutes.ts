fa2249f9466100d9e2c334ecdb3f761a07a9f48c
Ian <iangarciag@fb.com>
2022-07-19 18:41:51 -0700

    [CLIENT] Implemented new routes.
    Will be implemented in the near future.

--- client/src/routes/SidebarRoutes.ts
+++ client/src/routes/SidebarRoutes.ts
@@ -1,12 +1,33 @@
 import {iSidebarRoute} from "../types";
-import {HomeIcon} from "@heroicons/react/outline";
+import {BriefcaseIcon, HomeIcon, UserGroupIcon, UserIcon} from "@heroicons/react/outline";
 
+/**
+ * Routes the application is going to pull to render sidebar links.
+ */
 const SidebarRoutes: iSidebarRoute[] = [
     {
         path: "/app/",
         display: "Home",
         enabled: true,
         icon: HomeIcon
+    },
+    {
+        path: "/app/blog",
+        display: "Listings",
+        enabled: true,
+        icon: BriefcaseIcon
+    },
+    {
+        path: "/app/workers",
+        display: "Workers",
+        enabled: true,
+        icon: UserGroupIcon
+    },
+    {
+        path: "/app/profile",
+        display: "Profile",
+        enabled: true,
+        icon: UserIcon
     },
 ];
 
