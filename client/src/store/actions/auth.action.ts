c9579cc25365c2a497c909c054580c57e2bb3969
Ian <iangarciag@fb.com>
2022-07-19 18:42:12 -0700

    [CLIENT] [AUTH] Implemented authentication with Redux.

---
+++ client/src/store/actions/auth.action.ts
@@ -0,0 +1,62 @@
+import axios from "axios";
+
+const AUTH_ACTION_TYPE = {
+    LOGIN_SUCCESS: "LOGIN_SUCCESS",
+    LOGIN_FAIL: "LOGIN_FAIL",
+    LOGOUT_SUCCESS: "LOGOUT_SUCCESS"
+}
+
+const LoginAuthAction = (credentials: { email: string; password: string }, navigate: any) => {
+    return async (dispatch: any) => {
+        try {
+            await axios.post("http://localhost/login", credentials, {
+                headers: {
+                    "Content-Type": "application/json"
+                },
+            }).then(res => {
+                console.log(res);
+                const {data} = res;
+                if (data.success) {
+                    dispatch({
+                        type: AUTH_ACTION_TYPE.LOGIN_SUCCESS,
+                        payload: data
+                    });
+                    navigate("/app/")
+                } else {
+                    dispatch({
+                        type: AUTH_ACTION_TYPE.LOGIN_FAIL,
+                        payload: data
+                    });
+                }
+            }).catch(error => {
+                console.error(error);
+                if (error.response.data.error) {
+                    dispatch({
+                        type: AUTH_ACTION_TYPE.LOGIN_FAIL,
+                        payload: error.response.data.error
+                    });
+                }
+            });
+        } catch (error: any) {
+            console.error(error);
+            if (error.response.data.error) {
+                dispatch({
+                    type: AUTH_ACTION_TYPE.LOGIN_FAIL,
+                    payload: error.response.data.error
+                });
+            }
+        }
+    };
+}
+
+const LogoutAction = (navigate: any) => {
+    return (dispatch: any) => {
+        navigate("/");
+        dispatch({
+            type: AUTH_ACTION_TYPE.LOGOUT_SUCCESS,
+            payload: {}
+        });
+    };
+};
+
+export {AUTH_ACTION_TYPE, LoginAuthAction, LogoutAction};
