6ce8793519540a8d421bf93c4dd53c4371102492
Ian <iangarciag@fb.com>
2022-07-19 18:39:21 -0700

    [CLIENT] [AUTH] Added authentication validation.

--- client/src/containers/Layout.tsx
+++ client/src/containers/Layout.tsx
@@ -1,24 +1,31 @@
 import {lazy, Suspense, useContext, useEffect} from "react";
 import {SidebarContext} from "../context/SidebarContext";
-import {Navigate, Route, Routes, useLocation} from "react-router-dom";
+import {Route, Routes, useLocation, useNavigate} from "react-router-dom";
 import Wrapper from "./Wrapper";
 import routes from "../routes";
 import {iRoute} from "../types";
 import Sidebar from "../components/Sidebar";
 import TopBar from "../components/TopBar/TopBar";
 import Footer from "../components/Footer";
+import {connect} from "react-redux";
+import {mapStateToProps} from "../utils";
 
 const Error404 = lazy(() => import("./../pages/Error404"))
 
-const Layout = (): JSX.Element => {
+const Layout = ({auth}: any): JSX.Element => {
 
     // @ts-ignore
     const {isSidebarOpen, closeSidebar} = useContext(SidebarContext);
+    const navigate = useNavigate();
     let location = useLocation();
 
     useEffect(() => {
         closeSidebar();
     }, [closeSidebar, location]);
+
+    useEffect(() => {
+        if (!auth.isLoggedIn) navigate("/login");
+    }, [auth, navigate]);
 
     return <div className={`flex h-screen bg-gray-50 dark:bg-gray-900 ${isSidebarOpen && 'overflow-hidden'}`}>
         <Sidebar/>
@@ -33,7 +40,6 @@ const Layout = (): JSX.Element => {
                                 path={route.path}
                                 element={<route.component/>}
                             />)}
-                        <Route path="/" element={<Navigate to="/app/home"/>}/>
                         <Route path="*" element={<Error404/>}/>
                     </Routes>
                 </Suspense>
@@ -43,4 +49,4 @@ const Layout = (): JSX.Element => {
     </div>
 }
 
-export default Layout;
+export default connect(mapStateToProps, null)(Layout);
