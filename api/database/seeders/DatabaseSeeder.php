2962ee7d4b039184df89ec97b53d1ce87852f78b
Ian <iangarciag@fb.com>
2022-07-20 18:18:38 -0700

    [API] Added Factory triggers.

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -4,10 +4,12 @@ namespace Database\Seeders;
 
 use App\Models\Address;
 use App\Models\Bid;
+use App\Models\Category;
 use App\Models\Job;
 use App\Models\Offerer;
 use App\Models\Post;
 use App\Models\Rate;
+use App\Models\Service;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -21,11 +23,13 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(100)->create();
+        Category::factory(20)->create();
         Offerer::factory(60)->create();
         Address::factory(20)->create();
         Post::factory(50)->create();
         Bid::factory(1000)->create();
         Job::factory(50)->create();
         Rate::factory(30)->create();
+        Service::factory(100)->create();
     }
 }
