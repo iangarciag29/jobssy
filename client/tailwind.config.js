fd7977cbee2429cc769634de139f0d40a0147f7c
Ian <iangarciag@fb.com>
2022-07-21 17:53:39 -0700

    [CLIENT] Fixed tailwind file fetch.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/**/*.{js,jsx,ts,tsx}",
     "./public/*.{html,js,jsx,ts,tsx}",
     "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
