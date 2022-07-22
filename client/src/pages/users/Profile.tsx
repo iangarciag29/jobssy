cca9e32960a6208be2099a4cc48b1484a62b0145
Ian <iangarciag@fb.com>
2022-07-21 17:56:29 -0700

    [CLIENT] Added initial profile page.

---
+++ client/src/pages/users/Profile.tsx
@@ -0,0 +1,11 @@
+import Page from "../../containers/Page";
+
+const Profile = (): JSX.Element => {
+  return (
+    <Page title="Profile">
+      <h1>This is my profile page.</h1>
+    </Page>
+  );
+};
+
+export default Profile;
