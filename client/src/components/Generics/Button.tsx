9b5faec6e3e5126c405084dcb5dbabde6d781ad9
Ian <iangarciag@fb.com>
2022-07-21 17:51:43 -0700

    [CLIENT] Added additional className to button component.

--- client/src/components/Generics/Button.tsx
+++ client/src/components/Generics/Button.tsx
@@ -5,13 +5,20 @@ const Button = ({
   text,
   onClick,
   size = BTN_SIZE.SMALL,
+  className,
 }: {
   text: string;
   onClick?: () => void;
   size: BTN_SIZE;
+  className?: string;
 }): JSX.Element => {
   return (
-    <button onClick={onClick} className={`jobssy-btn ${translateSize(size)}`}>
+    <button
+      onClick={onClick}
+      className={`jobssy-btn ${translateSize(size)} ${
+        className ? className : ""
+      }`}
+    >
       {text}
     </button>
   );
