9483ed5b09fdddd9f1617ed3b87014b5a68e76a3
Ian <iangarciag@fb.com>
2022-07-21 17:51:28 -0700

    [CLIENT] Implemented initial user home dashboard.

---
+++ client/src/components/Content/UserHome.tsx
@@ -0,0 +1,52 @@
+// @ts-ignore
+import { graphql } from "babel-plugin-relay/macro";
+import { useLazyLoadQuery } from "react-relay";
+import { Link } from "react-router-dom";
+import {
+  UserHomeQuery,
+  UserHomeQuery$data,
+} from "./__generated__/UserHomeQuery.graphql";
+
+const UserHome = (): JSX.Element => {
+  const data: UserHomeQuery$data = useLazyLoadQuery<UserHomeQuery>(
+    graphql`
+      query UserHomeQuery {
+        offerers {
+          id
+          user {
+            id
+            first_name
+            last_name
+          }
+          start_time
+          description
+          jobs_completed
+          rating
+        }
+      }
+    `,
+    {},
+  );
+
+  const { offerers } = data;
+
+  return (
+    <div className="grid grid-cols-1 gap-5">
+      <h3>Find a worker now</h3>
+      {offerers?.map((offerer: any) => (
+        <Link
+          to={`/app/workers/${offerer.id}`}
+          key={offerer.id}
+          className="rounded-xl bg-white p-5 shadow"
+        >
+          <h4>
+            {offerer.user.first_name} [{offerer.id}]
+          </h4>
+          <p className="text-sm font-light">{offerer.description}</p>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default UserHome;
