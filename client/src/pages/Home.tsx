901648d9c87717305ee5b79f6e1adf488d505f8e
Ian <iangarciag@fb.com>
2022-07-21 17:55:11 -0700

    [CLIENT] Implemented Home component.
    Dynamically displays data based on the user.

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { Suspense } from "react";
+import Page from "../containers/Page";
+import { connect } from "react-redux";
+import { mapStateToProps } from "../utils";
+import UserHome from "../components/Content/UserHome";
 
-const Home = (): JSX.Element => {
-  //useLazyLoadQuery()
-
-  return <div className="m-20"></div>;
+const Home = ({ auth }: any): JSX.Element => {
+  return (
+    <Page title="Welcome to Jobssy">
+      <Suspense fallback="[DEBUG] [Home.tsx] Loading...">
+        {auth.user.is_offerer ? (
+          <h1>You have a worker profile.</h1>
+        ) : (
+          <UserHome />
+        )}
+      </Suspense>
+    </Page>
+  );
 };
 
-export default Home;
+export default connect(mapStateToProps, null)(Home);
