9a4261949bbbe088a68c064325d9206c3778a071
Ian <iangarciag@fb.com>
2022-07-21 17:54:47 -0700

    [CLIENT] Added Page container.
    Allows page to have a title and padding.

---
+++ client/src/containers/Page.tsx
@@ -0,0 +1,25 @@
+import { ChildrenElement } from "../types";
+
+/**
+ * Adjusts ChildrenElement type with custom page variables.
+ */
+interface PagePropType extends ChildrenElement {
+  title: string;
+}
+
+/**
+ * @param children Page content rendered as JSX.Element
+ * @param title Page title.
+ */
+const Page = ({ children, title }: PagePropType): JSX.Element => {
+  return (
+    <div className="p-10">
+      <div className="mb-10">
+        <h2 className="text-xl font-semibold">{title}</h2>
+      </div>
+      <div>{children}</div>
+    </div>
+  );
+};
+
+export default Page;
