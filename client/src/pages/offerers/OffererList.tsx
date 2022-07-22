36f208a93e3761b2aa390651ac37882c12ac7ae7
Ian <iangarciag@fb.com>
2022-07-21 17:55:21 -0700

    [CLIENT] Initial offerer list implemented.

---
+++ client/src/pages/offerers/OffererList.tsx
@@ -0,0 +1,11 @@
+import Page from "../../containers/Page";
+
+const OffererList = (): JSX.Element => {
+  return (
+    <Page title="Workers available">
+      <p>These are all the workers available.</p>
+    </Page>
+  );
+};
+
+export default OffererList;
