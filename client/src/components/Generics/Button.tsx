4a4437726a7fa6800879d5b0c5de7e200e62ad3e
Ian <iangarciag@fb.com>
2022-07-22 17:58:17 -0700

    [CLIENT] Implemented button type and disabled props.

--- client/src/components/Generics/Button.tsx
+++ client/src/components/Generics/Button.tsx
@@ -6,18 +6,24 @@ const Button = ({
   onClick,
   size = BTN_SIZE.SMALL,
   className,
+  type,
+  disabled = false,
 }: {
   text: string;
   onClick?: () => void;
   size: BTN_SIZE;
   className?: string;
+  type?: any;
+  disabled?: boolean;
 }): JSX.Element => {
   return (
     <button
       onClick={onClick}
+      type={type}
       className={`jobssy-btn ${translateSize(size)} ${
         className ? className : ""
       }`}
+      disabled={disabled}
     >
       {text}
     </button>
