bdb85456ddf281002e9f32647dabda390ca5f7c5
Ian <iangarciag@fb.com>
2022-07-22 18:10:47 -0700

    [CLIENT] Optimized file logic.
    Transfer button functionality to modal.

--- client/src/components/Modals/RequestJobModal.tsx
+++ client/src/components/Modals/RequestJobModal.tsx
@@ -1,37 +1,31 @@
 import { Modal } from "flowbite-react";
-import Button from "../Generics/Button";
-import { BTN_SIZE } from "../../types";
+import RequestServiceForm from "../Forms/RequestServiceForm";
+import { connect } from "react-redux";
+import { mapStateToProps } from "../../utils";
 
 const RequestJobModal = ({
   isModalOpen,
   setIsModalOpen,
   service,
+  auth,
 }: {
   isModalOpen: boolean;
   setIsModalOpen: (open: boolean) => void;
   service: any;
+  auth?: any;
 }): JSX.Element => {
   return (
     <Modal show={isModalOpen} onClose={() => setIsModalOpen(false)}>
       <Modal.Header>Request a new job</Modal.Header>
       <Modal.Body>
-        <div className="space-y-6">{service.title}</div>
+        <RequestServiceForm
+          service={service}
+          setIsModalOpen={setIsModalOpen}
+          user={auth.user}
+        />
       </Modal.Body>
-      <Modal.Footer>
-        <Button
-          onClick={() => setIsModalOpen(false)}
-          size={BTN_SIZE.MEDIUM}
-          text="Send request"
-        />
-        <Button
-          onClick={() => setIsModalOpen(false)}
-          size={BTN_SIZE.MEDIUM}
-          text="Cancel"
-          className="bg-red-700"
-        />
-      </Modal.Footer>
     </Modal>
   );
 };
 
-export default RequestJobModal;
+export default connect(mapStateToProps, null)(RequestJobModal);
