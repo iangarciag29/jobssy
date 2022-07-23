0c3e51a41160e85cc7ea5b62c299904c08542d79
Ian <iangarciag@fb.com>
2022-07-22 17:46:18 -0700

    [API] Get the offerer profile with the user id.
    [CAVEAT] User boolean `is_offerer` must match the existence of offerer profile.

---
+++ api/app/GraphQL/Queries/Offerer/OffererByUserQuery.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\GraphQL\Queries\Offerer;
+
+use App\Models\Offerer;
+use GraphQL\Type\Definition\Type;
+use GraphQL\Type\Definition\Type as GraphQLType;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Rebing\GraphQL\Support\Query;
+
+class OffererByUserQuery extends Query
+{
+
+    protected $attributes = [
+        'name' => 'offererByUser'
+    ];
+
+    public function type(): GraphQLType
+    {
+        return GraphQL::type('Offerer');
+    }
+
+    public function args(): array
+    {
+        return [
+            'id' => [
+                'name' => 'id',
+                'type' => Type::nonNull(Type::id()),
+                'rules' => ['required']
+            ]
+        ];
+    }
+
+    public function resolve($root, $args)
+    {
+        $id = $args['id'];
+
+        return Offerer::whereHas('user', function ($q) use ($id) {
+            $q->where('id', $id);
+        })->get();
+    }
+}
