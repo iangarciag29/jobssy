094ac343e4d2de122f4bbce58e64d33f266ca134
Ian <iangarciag@fb.com>
2022-07-25 12:53:43 -0700

    [CLIENT] Implemented new information into Job page.

--- client/src/pages/jobs/Job.tsx
+++ client/src/pages/jobs/Job.tsx
@@ -5,6 +5,9 @@ import { graphql } from "babel-plugin-relay/macro";
 import { JobQuery, JobQuery$data } from "./__generated__/JobQuery.graphql";
 import Page from "../../containers/Page";
 import { ClockIcon, CurrencyDollarIcon } from "@heroicons/react/outline";
+import StateToBadge from "../../utils/StateToBadge";
+import { Tooltip } from "flowbite-react";
+import JobStateTimeline from "../../components/Generics/Timeline/JobStateTimeline";
 
 const Job = (): JSX.Element => {
   const { id } = useParams();
@@ -34,13 +37,9 @@ const Job = (): JSX.Element => {
           }
           price
           currency
-          logs {
-            id
-            state_from
-            state_to
-            created_at
-          }
+          state
           created_at
+          ...JobStateTimeline_logs
         }
       }
     `,
@@ -53,25 +52,36 @@ const Job = (): JSX.Element => {
 
   return (
     <Page title={`Job - ${job.title}`}>
-      <div className="rounded-xl bg-white p-10 shadow">
-        <div className="flex flex-row">
-          <div className="flex w-full flex-col space-y-5 lg:w-2/3">
-            <span className="jobssy-span">Description</span>
-            <p>{job.description}</p>
+      <div className="flex flex-col space-y-10">
+        <div className="relative rounded-xl bg-white p-10 shadow">
+          <div className="flex flex-row">
+            <div className="flex w-full flex-col space-y-5 lg:w-2/3">
+              <span className="jobssy-span">Description</span>
+              <p>{job.description}</p>
+            </div>
+            <div className="flex w-full flex-col space-y-2 lg:w-1/3">
+              <span className="jobssy-span mb-4">Details</span>
+              <p className="inline-flex">
+                <CurrencyDollarIcon className="mt-1 mr-2 h-4 w-4" />
+                <span className="mr-5 font-semibold">Price:</span> ${job.price}{" "}
+                {job.currency}
+              </p>
+              <p className="inline-flex">
+                <ClockIcon className="mt-1 mr-2 h-4 w-4" />
+                <span className="mr-5 font-semibold">Created on:</span>
+                {job.created_at}
+              </p>
+            </div>
           </div>
-          <div className="flex w-full flex-col space-y-2 lg:w-1/3">
-            <span className="jobssy-span mb-4">Details</span>
-            <p className="inline-flex">
-              <CurrencyDollarIcon className="mt-1 mr-2 h-4 w-4" />
-              <span className="mr-5 font-semibold">Price:</span> ${job.price}{" "}
-              {job.currency}
-            </p>
-            <p className="inline-flex">
-              <ClockIcon className="mt-1 mr-2 h-4 w-4" />
-              <span className="mr-5 font-semibold">Created on:</span>
-              {job.created_at}
-            </p>
+          <div className="absolute top-5 right-5">
+            <Tooltip content="Job status">
+              <StateToBadge stateValue={job.state as string} />
+            </Tooltip>
           </div>
+        </div>
+        <div className="rounded-xl bg-white px-10 py-5 shadow">
+          <h4 className="mb-5 text-lg font-semibold">Timeline</h4>
+          <JobStateTimeline job={job} />
         </div>
       </div>
     </Page>
