89d45f181d3c371cd83ae54e005198bad3641a8f
Ian <iangarciag@fb.com>
2022-07-28 16:34:21 -0700

    [CLIENT] Implemented optional page top right button.

--- client/src/containers/Page.tsx
+++ client/src/containers/Page.tsx
@@ -5,17 +5,20 @@ import { ChildrenElement } from "../types";
  */
 interface PagePropType extends ChildrenElement {
   title: string | JSX.Element;
+  actionBtn?: JSX.Element;
 }
 
 /**
  * @param children Page content rendered as JSX.Element
  * @param title Page title.
+ * @param button Action button displayed at the top of the page.
  */
-const Page = ({ children, title }: PagePropType): JSX.Element => {
+const Page = ({ children, title, actionBtn }: PagePropType): JSX.Element => {
   return (
     <div className="p-10">
-      <div className="mb-10">
+      <div className="mb-10 flex flex-row justify-between">
         <h2 className="text-xl font-semibold">{title}</h2>
+        {actionBtn}
       </div>
       <div>{children}</div>
     </div>
