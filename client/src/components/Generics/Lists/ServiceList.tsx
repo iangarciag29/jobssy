f729fb5e1338831a9c1d6e252abddb884068a15f
Ian <iangarciag@fb.com>
2022-08-01 18:35:11 -0700

    [CLIENT] Service list message feedback.

--- client/src/components/Generics/Lists/ServiceList.tsx
+++ client/src/components/Generics/Lists/ServiceList.tsx
@@ -26,6 +26,7 @@ const ServiceList = ({ offerer }: { offerer: any }): JSX.Element => {
             id
           }
           address {
+            id
             name
           }
           category {
@@ -39,45 +40,53 @@ const ServiceList = ({ offerer }: { offerer: any }): JSX.Element => {
 
   const { services } = data;
 
+  if (!services || services.length <= 0)
+    return (
+      <div className="py-5 text-center">
+        <p className="text-gray-500">This worker hasn't posted any services.</p>
+      </div>
+    );
+
   return (
     <div className="flex flex-col space-y-10">
-      {services?.map((service: any) => (
-        <div
-          className="flex flex-row justify-between space-x-5"
-          key={service.id}
-        >
-          <div className="mr-10 space-y-5">
-            <h5 className="text-xl font-bold capitalize text-secondary">
-              {service.title}
-            </h5>
-            <div className="inline-flex">
-              <Badge color="indigo">{service.category.name}</Badge>
+      {services &&
+        services.map((service: any) => (
+          <div
+            className="flex flex-row justify-between space-x-5"
+            key={service.id}
+          >
+            <div className="mr-10 space-y-5">
+              <h5 className="text-xl font-bold capitalize text-secondary">
+                {service.title}
+              </h5>
+              <div className="inline-flex">
+                <Badge color="indigo">{service.category.name}</Badge>
+              </div>
+              <p className="text-justify">{service.description}</p>
             </div>
-            <p className="text-justify">{service.description}</p>
+            <div className="grid flex-1 items-center">
+              <div className="flex flex-col text-center text-jobssy-blue">
+                <span className="text-2xl font-black">${service.price}</span>{" "}
+                <span className="font-semibold">{service.currency}</span>
+              </div>
+              <div className="text-center">
+                <Button
+                  size={BTN_SIZE.SMALL}
+                  text="Request"
+                  onClick={() => {
+                    setSelectedService(service);
+                    setIsNewJobModalOpen(true);
+                  }}
+                />
+              </div>
+            </div>
+            <RequestJobModal
+              isModalOpen={isNewJobModalOpen}
+              setIsModalOpen={setIsNewJobModalOpen}
+              service={selectedService}
+            />
           </div>
-          <div className="grid flex-1 items-center">
-            <div className="flex flex-col text-center text-jobssy-blue">
-              <span className="text-2xl font-black">${service.price}</span>{" "}
-              <span className="font-semibold">{service.currency}</span>
-            </div>
-            <div className="text-center">
-              <Button
-                size={BTN_SIZE.SMALL}
-                text="Request"
-                onClick={() => {
-                  setSelectedService(service);
-                  setIsNewJobModalOpen(true);
-                }}
-              />
-            </div>
-          </div>
-          <RequestJobModal
-            isModalOpen={isNewJobModalOpen}
-            setIsModalOpen={setIsNewJobModalOpen}
-            service={selectedService}
-          />
-        </div>
-      ))}
+        ))}
     </div>
   );
 };
