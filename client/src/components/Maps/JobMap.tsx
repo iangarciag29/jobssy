58223e2d84e427661a89602a5cc99fb887eea09a
Ian <iangarciag@fb.com>
2022-08-01 18:35:44 -0700

    [CLIENT] [GMAPS] Fixed custom address map display.

--- client/src/components/Maps/JobMap.tsx
+++ client/src/components/Maps/JobMap.tsx
@@ -1,16 +1,12 @@
-import { GoogleMap } from "@react-google-maps/api";
+import { GoogleMap, Marker } from "@react-google-maps/api";
 import { useCallback, useMemo, useRef } from "react";
 
 type LatLngLiteral = google.maps.LatLngLiteral;
-type DirectionsResult = google.maps.DirectionsResult;
 type MapOptions = google.maps.MapOptions;
 
-const JobMap = (): JSX.Element => {
+const JobMap = ({ address }: any): JSX.Element => {
   const mapRef = useRef<GoogleMap>();
-  const center = useMemo<LatLngLiteral>(
-    () => ({ lat: 37.3551616, lng: -122.0177252 }),
-    [],
-  );
+
   const options = useMemo<MapOptions>(
     () => ({
       mapId: "ad6644a2d5ec6ecb",
@@ -22,26 +18,27 @@ const JobMap = (): JSX.Element => {
 
   const onLoad = useCallback((map: any) => (mapRef.current = map), []);
 
+  const center = useMemo<LatLngLiteral>(
+    () => ({
+      lat: address.latitude,
+      lng: address.longitude,
+    }),
+    [address],
+  );
+
   return (
     <div className="h-96">
       <GoogleMap
-        zoom={14}
+        zoom={6}
         center={center}
         mapContainerStyle={{ width: "100%", height: "100%" }}
         options={options}
         onLoad={onLoad}
-      ></GoogleMap>
+      >
+        <Marker position={center} />
+      </GoogleMap>
     </div>
   );
-};
-
-const mapOptions = {
-  strokeOpacity: 0.5,
-  strokeWeight: 2,
-  clickable: false,
-  draggable: true,
-  editable: false,
-  visible: true,
 };
 
 export default JobMap;
