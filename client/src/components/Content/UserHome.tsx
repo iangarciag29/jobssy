f84084a267bc0443d6cbe03467b1cbff78d72268
Ian <iangarciag@fb.com>
2022-08-01 18:33:53 -0700

    [CLIENT] Implemented new ad into user home page.

--- client/src/components/Content/UserHome.tsx
+++ client/src/components/Content/UserHome.tsx
@@ -6,6 +6,7 @@ import {
   UserHomeQuery,
   UserHomeQuery$data,
 } from "./__generated__/UserHomeQuery.graphql";
+import OffererAd from "../Alerts/OffererAd";
 
 const UserHome = (): JSX.Element => {
   const data: UserHomeQuery$data = useLazyLoadQuery<UserHomeQuery>(
@@ -31,20 +32,23 @@ const UserHome = (): JSX.Element => {
   const { offerers } = data;
 
   return (
-    <div className="grid grid-cols-1 gap-5">
-      <h3>Find a worker now</h3>
-      {offerers?.map((offerer: any) => (
-        <Link
-          to={`/app/workers/${offerer.id}`}
-          key={offerer.id}
-          className="rounded-xl bg-white p-5 shadow"
-        >
-          <h4>
-            {offerer.user.first_name} [{offerer.id}]
-          </h4>
-          <p className="text-sm font-light">{offerer.description}</p>
-        </Link>
-      ))}
+    <div>
+      <OffererAd />
+      <div className="grid grid-cols-1 gap-5">
+        <h3>Find a worker now</h3>
+        {offerers?.map((offerer: any) => (
+          <Link
+            to={`/app/workers/${offerer.id}`}
+            key={offerer.id}
+            className="rounded-xl bg-white p-5 shadow"
+          >
+            <h4>
+              {offerer.user.first_name} [{offerer.id}]
+            </h4>
+            <p className="text-sm font-light">{offerer.description}</p>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
