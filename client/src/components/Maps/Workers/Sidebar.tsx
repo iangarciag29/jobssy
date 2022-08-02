11aa4121fe1a5e7f4ecd5871f9d061865301e721
Ian <iangarciag@fb.com>
2022-08-02 13:47:21 -0700

    [CLIENT] Improved offerer list info.

--- client/src/components/Maps/Workers/Sidebar.tsx
+++ client/src/components/Maps/Workers/Sidebar.tsx
@@ -1,4 +1,7 @@
 import { useNavigate } from "react-router-dom";
+import { CheckCircleIcon } from "@heroicons/react/outline";
+import { Tooltip } from "flowbite-react";
+import * as timeago from "timeago.js";
 
 const Sidebar = ({ offerers, selectOfferer }: any): JSX.Element => {
   const navigate = useNavigate();
@@ -6,17 +9,28 @@ const Sidebar = ({ offerers, selectOfferer }: any): JSX.Element => {
   return (
     <div>
       <h4 className="text-center text-xl font-semibold">Workers available</h4>
+      <hr className="mx-10 mt-10" />
       <div className="mt-5 flex max-h-full flex-col justify-start overflow-auto">
         {offerers.map((offerer: any, idx: number) => (
           <div
             key={offerer.id}
-            className={`p-5 hover:cursor-pointer ${
+            className={`relative p-5 hover:z-20 hover:cursor-pointer hover:border-r-4 hover:border-jobssy-blue hover:shadow ${
               idx % 2 === 0 ? "" : "bg-white"
             }`}
             onClick={() => navigate(`/app/workers/${offerer.id}`)}
           >
-            <h5 className="font-semibold">{offerer.user.first_name}</h5>
-            <p>{offerer.description}</p>
+            <h5 className="inline-flex font-semibold">
+              {offerer.user.first_name}{" "}
+              {offerer.user.verified && (
+                <Tooltip content="Verified">
+                  <CheckCircleIcon className="ml-2 h-3 w-3" />
+                </Tooltip>
+              )}
+            </h5>
+            <p className="truncate text-sm">{offerer.description}</p>
+            <span className="jobssy-span text-jobssy-blue">
+              Joined {timeago.format(offerer.start_time)}
+            </span>
           </div>
         ))}
       </div>
