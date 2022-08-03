50daa20c7959e65d1606c4301207167fa1532978
Ian <iangarciag@fb.com>
2022-08-02 17:32:03 -0700

    [CLIENT] Added internal filtering array.

--- client/src/components/Maps/Workers/Sidebar.tsx
+++ client/src/components/Maps/Workers/Sidebar.tsx
@@ -2,16 +2,34 @@ import { useNavigate } from "react-router-dom";
 import { CheckCircleIcon } from "@heroicons/react/outline";
 import { Tooltip } from "flowbite-react";
 import * as timeago from "timeago.js";
+import { useEffect, useState } from "react";
+import { isWithinBounds } from "../../../utils/MathUtils";
 
 const Sidebar = ({ offerers, bounds }: any): JSX.Element => {
   const navigate = useNavigate();
+
+  const [internalFilter, setInternalFilter] = useState(offerers);
+
+  useEffect(() => {
+    setInternalFilter(
+      offerers.filter((offerer: any) =>
+        isWithinBounds(
+          {
+            lat: offerer.user.address.latitude,
+            lng: offerer.user.address.longitude,
+          },
+          bounds,
+        ),
+      ),
+    );
+  }, [bounds, offerers]);
 
   return (
     <div>
       <h4 className="text-center text-xl font-semibold">Workers available</h4>
       <hr className="mx-10 mt-10" />
       <div className="mt-5 flex max-h-full flex-col justify-start overflow-auto">
-        {offerers.map((offerer: any, idx: number) => (
+        {internalFilter.map((offerer: any, idx: number) => (
           <div
             key={offerer.id}
             className={`relative p-5 hover:z-20 hover:cursor-pointer hover:border-r-4 hover:border-jobssy-blue hover:shadow ${
