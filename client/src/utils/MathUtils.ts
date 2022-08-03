205de9778763bec420b877b81089f9ec214a060a
Ian <iangarciag@fb.com>
2022-08-02 17:33:25 -0700

    [CLIENT] Improved math null verification.

--- client/src/utils/MathUtils.ts
+++ client/src/utils/MathUtils.ts
@@ -1,5 +1,11 @@
 type LatLngLiteral = google.maps.LatLngLiteral;
 
 export const isWithinBounds = (marker: LatLngLiteral, bounds: any): boolean => {
-  return true;
+  if (!marker.lat || !marker.lng || !bounds.ne || !bounds.sw) return false;
+  return (
+    marker.lat >= bounds.sw.lat &&
+    marker.lat <= bounds.ne.lat &&
+    marker.lng >= bounds.sw.lng &&
+    marker.lng <= bounds.ne.lng
+  );
 };
