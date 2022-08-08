07621cae1cf028f079ef0438a5e05fa5cd8a1236
Ian <iangarciag@fb.com>
2022-08-08 15:19:54 -0700

    [CLIENT] Changed sidebar background color.

--- client/src/components/Sidebar/SidebarContent.tsx
+++ client/src/components/Sidebar/SidebarContent.tsx
@@ -11,7 +11,7 @@ const SidebarContent = ({ auth }: any): JSX.Element => {
   return (
     <div className="py-4 text-gray-500">
       <div className="my-10 text-center">
-        <Link to="/app/" className="text-3xl font-bold text-primary">
+        <Link to="/app/" className="text-3xl font-bold text-white">
           Jobssy
         </Link>
       </div>
@@ -23,7 +23,7 @@ const SidebarContent = ({ auth }: any): JSX.Element => {
               <li className="relative px-6 py-3" key={idx}>
                 <NavLink
                   to={route.path}
-                  className="inline-flex w-full items-center text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                  className="inline-flex w-full items-center text-sm font-semibold text-gray-400 transition-colors duration-150 hover:text-gray-300"
                 >
                   <route.icon className="h-5 w-5" />
                   <span className="ml-4">{route.display}</span>
--- client/src/components/Sidebar/Versions/Desktop.tsx
+++ client/src/components/Sidebar/Versions/Desktop.tsx
@@ -2,7 +2,7 @@ import SidebarContent from "../SidebarContent";
 
 const Desktop = (): JSX.Element => {
   return (
-    <aside className="z-40 hidden w-64 flex-shrink-0 overflow-y-auto bg-white lg:block">
+    <aside className="z-40 hidden w-64 flex-shrink-0 overflow-y-auto bg-primary text-gray-50 lg:block">
       <SidebarContent />
     </aside>
   );
