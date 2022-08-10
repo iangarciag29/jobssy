c6e1802192b0f91f186c2d0fffc08969db0e26d5
Ian <iangarciag@fb.com>
2022-08-10 11:52:09 -0700

    [CLIENT] Reverse timeline logs.

--- client/src/components/Generics/Timeline/JobStateTimeline.tsx
+++ client/src/components/Generics/Timeline/JobStateTimeline.tsx
@@ -29,26 +29,28 @@ const JobStateTimeline = ({ job }: any): JSX.Element => {
   if (logs.length > 0) {
     return (
       <ol className="relative border-l border-gray-200 dark:border-gray-700">
-        {logs.map((log: any) => (
-          <li className="mb-10 ml-6" key={log.id}>
-            <span className="absolute -left-3 flex h-6 w-6 items-center justify-center rounded-full bg-jobssy-blue ring-8 ring-white dark:bg-blue-900 dark:ring-gray-900">
-              <ClockIcon className="h-5 w-5 text-gray-50" />
-            </span>
-            <div className="items-center justify-between rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-600 dark:bg-gray-700 sm:flex">
-              <time className="mb-1 inline-flex text-xs font-normal text-gray-400 sm:order-last sm:mb-0">
-                <Tooltip content={log.created_at}>
-                  <ClockIcon className="mt-0.5 mr-1 h-3 w-3" />
-                </Tooltip>
-                {timeago.format(log.created_at)}
-              </time>
-              <div className="text-sm font-normal text-gray-500 dark:text-gray-300">
-                The status changed from{" "}
-                <StateToBadge stateValue={log.state_from as string} /> to{" "}
-                <StateToBadge stateValue={log.state_to as string} />.
+        {Array.from(logs)
+          .reverse()
+          .map((log: any) => (
+            <li className="mb-10 ml-6" key={log.id}>
+              <span className="absolute -left-3 flex h-6 w-6 items-center justify-center rounded-full bg-jobssy-blue ring-8 ring-white dark:bg-blue-900 dark:ring-gray-900">
+                <ClockIcon className="h-5 w-5 text-gray-50" />
+              </span>
+              <div className="items-center justify-between rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-600 dark:bg-gray-700 sm:flex">
+                <time className="mb-1 inline-flex text-xs font-normal text-gray-400 sm:order-last sm:mb-0">
+                  <Tooltip content={log.created_at}>
+                    <ClockIcon className="mt-0.5 mr-1 h-3 w-3" />
+                  </Tooltip>
+                  {timeago.format(log.created_at)}
+                </time>
+                <div className="text-sm font-normal text-gray-500 dark:text-gray-300">
+                  The status changed from{" "}
+                  <StateToBadge stateValue={log.state_from as string} /> to{" "}
+                  <StateToBadge stateValue={log.state_to as string} />.
+                </div>
               </div>
-            </div>
-          </li>
-        ))}
+            </li>
+          ))}
       </ol>
     );
   } else {
