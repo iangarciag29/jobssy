ad0e768f63e288960107ed41699ae339f0ca62aa
Ian <iangarciag@fb.com>
2022-08-09 18:33:24 -0700

    [CLIENT] Added listing link in card.

--- client/src/components/Generics/Lists/PostList.tsx
+++ client/src/components/Generics/Lists/PostList.tsx
@@ -1,8 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
 const PostList = ({ posts }: any): JSX.Element => {
+  const navigate = useNavigate();
+
   return (
-    <div className="grid gap-5">
+    <div className="grid grid-cols-1 gap-5 lg:grid-cols-3">
       {posts.map((post: any) => (
-        <div key={post.id} className="rounded-xl bg-white p-10 shadow">
+        <div
+          key={post.id}
+          className="rounded-xl bg-white p-10 shadow hover:cursor-pointer"
+          onClick={() => navigate(`/app/listings/${post.id}`)}
+        >
           <h4 className="text-lg font-semibold">{post.title}</h4>
           {post.description}
         </div>
