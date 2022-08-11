4a3868f559aeb3e5cd967add5735ebf49df5d585
Ian <iangarciag@fb.com>
2022-08-11 15:47:30 -0700

    [API] Registered new mutations/queries.

--- api/app/Models/Rate.php
+++ api/app/Models/Rate.php
@@ -13,14 +13,15 @@ class Rate extends Model
 
     protected $primaryKey = 'id';
 
-    protected $fillable = ['value', 'comment', 'anonymous'];
+    protected $fillable = ['offerer_id', 'job_id', 'value', 'comment', 'anonymous'];
 
     public function job(): \Illuminate\Database\Eloquent\Relations\HasOne
     {
         return $this->hasOne(Job::class, 'rate_id', 'id');
     }
 
-    public function offerer() {
+    public function offerer()
+    {
         return $this->belongsTo(Offerer::class, 'offerer_id');
     }
 }
