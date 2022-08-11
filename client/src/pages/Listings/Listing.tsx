6ce868cbb28d01af98ecf5b25ef09033cda9ecb1
Ian <iangarciag@fb.com>
2022-08-11 15:53:16 -0700

    [CLIENT] Improved listings page data preview.

--- client/src/pages/Listings/Listing.tsx
+++ client/src/pages/Listings/Listing.tsx
@@ -1,6 +1,6 @@
 import Page from "../../containers/Page";
 import { useNavigate, useParams } from "react-router-dom";
-import { useLazyLoadQuery } from "react-relay";
+import { useLazyLoadQuery, useMutation } from "react-relay";
 // @ts-ignore
 import { graphql } from "babel-plugin-relay/macro";
 import {
@@ -18,11 +18,18 @@ import BasicProfile from "../../components/Generics/Cards/BasicProfile";
 import { connect } from "react-redux";
 import { mapStateToProps } from "../../utils";
 import * as timeago from "timeago.js";
-import { Tooltip } from "flowbite-react";
+import { Badge, Tooltip } from "flowbite-react";
+import { useState } from "react";
+import NewBidModal from "../../components/Modals/NewBidModal";
+import { HandleGraphQLError } from "../../utils/ErrorHandler";
+import { AlertHandler } from "../../utils/AlertHandler";
 
 const Listing = ({ auth }: { auth?: any }): JSX.Element => {
   const { id }: any = useParams();
   const navigate = useNavigate();
+
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [fetchKey, setFetchKey] = useState<number>(0);
 
   const data: ListingQuery$data = useLazyLoadQuery<ListingQuery>(
     graphql`
@@ -33,6 +40,7 @@ const Listing = ({ auth }: { auth?: any }): JSX.Element => {
           description
           price
           currency
+          visible
           user {
             id
             first_name
@@ -41,6 +49,9 @@ const Listing = ({ auth }: { auth?: any }): JSX.Element => {
             email
             cellphone
             created_at
+            address {
+              id
+            }
           }
           bids {
             id
@@ -66,7 +77,76 @@ const Listing = ({ auth }: { auth?: any }): JSX.Element => {
       }
     `,
     { id },
+    { fetchKey, fetchPolicy: "network-only" },
   );
+
+  const [commitCreateJobMutation] = useMutation(graphql`
+    mutation ListingMutation(
+      $title: String!
+      $description: String!
+      $user_id: ID!
+      $offerer_id: ID!
+      $currency: String!
+      $price: Float!
+      $address_id: ID!
+    ) {
+      createJob(
+        title: $title
+        description: $description
+        user_id: $user_id
+        offerer_id: $offerer_id
+        currency: $currency
+        price: $price
+        address_id: $address_id
+        started_by_offerer: false
+      ) {
+        id
+      }
+    }
+  `);
+
+  const [commitHidePostMutation] = useMutation(graphql`
+    mutation ListingHideMutation($id: ID!) {
+      togglePostVisibility(id: $id)
+    }
+  `);
+
+  const acceptBid = (id: any): void => {
+    if (!post || !post.user || !post.user.address) return;
+    commitCreateJobMutation({
+      variables: {
+        title: post.title,
+        description: post.description,
+        user_id: post.user.id,
+        offerer_id: auth.user.id,
+        currency: post.currency,
+        price: post.price,
+        address_id: post.user.address.id,
+      },
+      onCompleted: (response: any, errors) => {
+        if (!HandleGraphQLError(errors)) return;
+        const { createJob } = response;
+        AlertHandler.fire({
+          icon: "success",
+          title: "Success",
+          text: "You accepted the bid.",
+        }).then((_) => {
+          commitHidePostMutation({
+            variables: {
+              id: post.id,
+            },
+            onCompleted: (response1, errors1) => {
+              if (!HandleGraphQLError(errors1)) return;
+              navigate(`/app/jobs/${createJob.id}`);
+            },
+          });
+        });
+      },
+      onError: (error) => {
+        console.error(error);
+      },
+    });
+  };
 
   const { post } = data;
 
@@ -74,8 +154,6 @@ const Listing = ({ auth }: { auth?: any }): JSX.Element => {
     navigate("/app/listings");
     return <></>;
   }
-
-  console.log(post);
 
   return (
     <Page
@@ -90,9 +168,16 @@ const Listing = ({ auth }: { auth?: any }): JSX.Element => {
     >
       <div className="flex flex-col space-y-10 space-x-0 px-10 lg:flex-row lg:space-y-0 lg:space-x-10">
         <div className="flex w-full flex-col space-y-10 lg:w-2/3">
-          <div className="jobssy-bubble flex flex-col space-y-5 px-10 py-5">
+          <div className="jobssy-bubble relative flex flex-col space-y-5 px-10 py-5">
             <h4 className="text-xl font-semibold">Author</h4>
             <BasicProfile profile={post.user} />
+            {!post.visible ? (
+              <div className="absolute top-0 right-5">
+                <Badge color="gray">Hidden post</Badge>
+              </div>
+            ) : (
+              <></>
+            )}
           </div>
           <div className="jobssy-bubble relative flex flex-col space-y-10 p-10">
             <span className="jobssy-span">Description</span>
@@ -117,7 +202,7 @@ const Listing = ({ auth }: { auth?: any }): JSX.Element => {
             <h4 className="text-center text-xl font-semibold">
               Bids for this job
             </h4>
-            {auth.user.id !== post.user?.id && (
+            {auth.user.id !== post.user?.id && auth.user.is_offerer ? (
               <div className="mt-5 w-full text-center">
                 <Button
                   text={
@@ -128,24 +213,70 @@ const Listing = ({ auth }: { auth?: any }): JSX.Element => {
                       Make a bid
                     </p>
                   }
+                  onClick={() => setModalOpen(true)}
                   className="bg-green-700 text-green-50"
                   size={BTN_SIZE.SMALL}
                 />
               </div>
+            ) : (
+              <></>
             )}
             <hr className="my-5" />
-            {post.bids && post.bids.length <= 0 ? (
+            {(post.bids && post.bids.length <= 0) || !post.visible ? (
               <p className="mt-10 text-center text-gray-600">
-                There are now bids for this job.
+                There are now bids for this listing.
               </p>
             ) : (
-              post.bids?.map((bid: any) => (
-                <div>{bid.offerer.user.first_name}</div>
-              ))
+              <div className="mt-10 flex flex-col space-y-10">
+                {post &&
+                  post.bids?.map((bid: any, index: number) => (
+                    <div key={bid.id}>
+                      <div className="flex flex-row justify-between">
+                        <div className="flex flex-col">
+                          <h5>
+                            {bid.offerer.user.first_name}{" "}
+                            {bid.offerer.user.last_name}
+                          </h5>
+                          <p className="inline-flex text-gray-500">
+                            <span>
+                              <ClockIcon className="mr-1 mt-1 h-3 w-3" />
+                            </span>
+                            {timeago.format(bid.created_at)}
+                          </p>
+                          {auth.user.id === post.user?.id ? (
+                            <Button
+                              text="Accept bid"
+                              size={BTN_SIZE.SMALL}
+                              className="mt-2"
+                              onClick={() => acceptBid(bid.id)}
+                            />
+                          ) : (
+                            <></>
+                          )}
+                        </div>
+                        <div className="grid items-center text-center text-jobssy-blue">
+                          <p className="text-3xl font-black">${bid.amount}</p>
+                          <span>{bid.currency}</span>
+                        </div>
+                      </div>
+                      {post && post.bids && index !== post.bids.length - 1 ? (
+                        <hr className="mt-10" />
+                      ) : (
+                        <></>
+                      )}
+                    </div>
+                  ))}
+              </div>
             )}
           </div>
         </div>
       </div>
+      <NewBidModal
+        post={post}
+        openModal={modalOpen}
+        setOpenModal={setModalOpen}
+        setFetchKey={setFetchKey}
+      />
     </Page>
   );
 };
